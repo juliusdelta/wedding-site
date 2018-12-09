11cb2b2bbc1c25cc865cb1d294ddfa27cf90d529
JD Gonzales <jd_gonzales@icloud.com>
2018-12-09 12:54:19 -0600

    Update reminder Cron

--- backend/src/index.js
+++ backend/src/index.js
@@ -50,20 +50,24 @@ cron.schedule('00 30 15 27 11 *', () => {
   timezone: "America/Chicago"
 });
 
-// cron.schedule('30 19 2 12 0', () => {
-//   console.log('REMINDER INVITE CRON STARTING');
-//   const message = "Reminder to RSVP to Jonathan & Julie's wedding! Click here: https://gonzaleswedding.com" 
-//   db.query.persons({ where: { reserved: false } }, `
-//   { 
-//     phoneNumber
-//   }
-//   `).then(record => {
-//     record.forEach(record => {
-//       sendMessage(record.phoneNumber, message)
-//     });
-//   });
-//   console.log('REMINDER INVITE CRON ENDING');
-// })
+cron.schedule('00 30 15 10 12 *', () => {
+  console.log('====================================REMINDER CRON STARTING ====================================');
+  const message = "Reminder to make your reservation at for Jonathan Gonzales and Julie Morrison's wedding! Click here to make your reservation: www.gonzaleswedding.com"
+  db.query.persons({ where: { reserved: false, submitted: false, phoneNumber_not: "", optedOut: false } }, `
+  { 
+    phoneNumber
+  }
+  `).then(record => {
+      record.forEach(record => {
+        console.log('Sending message to: ' + record.phoneNumber);
+        sendMessage(record.phoneNumber, message)
+      });
+    });
+  sendMessage('8177347453', "Your reminders have been sent! www.gonzaleswedding.com");
+  console.log('====================================REMINDER CRON ENDING ====================================');
+}, {
+  timezone: "America/Chicago"
+});
 
 server.start({
   cors: {
